fef0749af472028a0dd88502904542cf61097d01
Rodion Mostovoi <36400912+rodion-m@users.noreply.github.com>
2024-04-07 02:40:42 +0700

    Update SVG icon path to absolute URL
    Instead of using a relative path for the SVG icons, the code now uses an absolute URL. This change includes the window.location.origin to ensure that the SVG icon loads correctly, regardless of the application's base path.

--- src/common/SvgIcon/index.tsx
+++ src/common/SvgIcon/index.tsx
@@ -1,5 +1,5 @@
 import { SvgIconProps } from "../types";
 
 export const SvgIcon = ({ src, width, height }: SvgIconProps) => (
-  <img src={`/img/svg/${src}`} alt={src} width={width} height={height} />
+  <img src={window.location.origin + `/img/svg/${src}`} alt={src} width={width} height={height} />
 );
